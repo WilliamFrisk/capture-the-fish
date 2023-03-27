3fe427e9b4320d26d882e5ea1a3a9128f12eb46a
William <william.hp.frisk@gmail.com>
2023-03-27 13:04:15 +0200

    första commiten

---
+++ code/desktop/src/com/group11/ctfish/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.group11.ctfish;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.group11.ctfish.CtFish;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("capture-the-fish");
+		new Lwjgl3Application(new CtFish(), config);
+	}
+}
