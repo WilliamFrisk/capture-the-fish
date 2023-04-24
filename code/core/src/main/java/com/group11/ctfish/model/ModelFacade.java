5a8f7dd154c0b57157c825dfbc289fc243a1777e
fannysj <93663750+fannysj@users.noreply.github.com>
2023-04-24 16:02:30 +0200

    fix
    fix m import

--- code/core/src/main/java/com/group11/ctfish/model/ModelFacade.java
+++ code/core/src/main/java/com/group11/ctfish/model/ModelFacade.java
@@ -1,5 +1,6 @@
 package com.group11.ctfish.model;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.group11.ctfish.model.fish.Fish;
 import com.group11.ctfish.model.fish.FishFactory;
 import com.group11.ctfish.model.fish.properties.FishProperty;
