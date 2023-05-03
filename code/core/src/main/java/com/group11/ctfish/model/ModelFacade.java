9503d128c75e2ff1a55022a44e1bb0511023b486
majalar <majalar@student.chalmers.se>
2023-05-03 12:54:34 +0200

    question screen

--- code/core/src/main/java/com/group11/ctfish/model/ModelFacade.java
+++ code/core/src/main/java/com/group11/ctfish/model/ModelFacade.java
@@ -5,6 +5,7 @@ import com.group11.ctfish.model.fish.Fish;
 import com.group11.ctfish.model.fish.FishFactory;
 import com.group11.ctfish.model.fish.properties.FishProperty;
 import com.group11.ctfish.model.fish.sizes.FishSize;
+import com.group11.ctfish.model.quiz.QuizLogic;
 
 import com.group11.ctfish.model.user.User;
 
@@ -13,11 +14,13 @@ import com.sun.org.apache.xpath.internal.operations.Mod;
 
 
 import java.util.List;
+import java.util.Set;
 
 public class ModelFacade {
     private List<Fish> fishList;
     private static ModelFacade instance = new ModelFacade();
     private ModelFacade (){}
+    QuizLogic QL = new QuizLogic();
 
 
     public static ModelFacade getInstance(){
@@ -37,5 +40,21 @@ public class ModelFacade {
     private void createFish(int x, int y, FishProperty property, Sizes size, Texture texture) {
 
         fishList.add(FishFactory.createFish(x,y,property, size, texture));
+    }
+
+    public String getSpecificAnswer(int integer){
+        return QL.getSpecificAnswer(integer);
+    }
+
+    public String getQuestion(){
+        return QL.getQuestion();
+    }
+
+    public Set<String> getAnswers(){
+        return QL.getAnswers();
+    }
+
+    public String getRightAnswer(){
+        return QL.getRightAnswer();
     }
 }
