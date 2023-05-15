88c277d01fb0b8bf4c509331a87c2c48dd51ad10
William <william.hp.frisk@gmail.com>
2023-05-15 11:18:56 +0200

    bug fix

--- code/core/src/main/java/com/group11/ctfish/view/FrontScreen.java
+++ code/core/src/main/java/com/group11/ctfish/view/FrontScreen.java
@@ -102,7 +102,10 @@ public class FrontScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        stage.act(delta);
         stage.draw();
+
+
     }
 
     @Override
